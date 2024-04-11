3bf2fd95a9fc4b5b09bbc114efb682b6dc38d7dd
Abhinav Kumar <abhinavkr.2108@gmail.com>
2024-04-11 17:11:49 +0530

    Vite config changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/abhinavkr2108/react-flow/",
+
   server: {
     watch: {
       usePolling: true,
